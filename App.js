ccb767eec836fd4544ea144e90251aea280c2b76
Daniel <danyel.nerv@gmail.com>
2018-08-08 13:15:43 -0500

    Add demo of custom components

--- App.js
+++ App.js
@@ -17,19 +17,25 @@ import marker from './images/marker.png';
 //Roulette numbers
 const numbers = [0,32,15,19,4,21,2,25,17,34,6,27,13,36,11,30,8,23,10,5,24,16,33,1,20,14,31,9,22,18,29,7,28,12,35,3,26]
 const options  = numbers.map((o)=>({index:o}))  
-
+const customOptions = numbers.map((o)=> (
+  <Text index={o}>{o}</Text>
+));
 export default class App extends Component {
   constructor(props){
     super(props);
     this.onRotate = this.onRotate.bind(this);
     this.onRotateChange = this.onRotateChange.bind(this);
+    this.onRotateCustom = this.onRotateCustom.bind(this);
+    this.onRotateCustomChange = this.onRotateCustomChange.bind(this);
     this.state={
       option:"Option selected:",
-      rouletteState:'stop'
+      optionCustom:"Option selected:",
+      rouletteState:'stop',
+      rouletteCustomState:'stop'
     }
   }
   render() {
-    const{option, rouletteState} = this.state
+    const{option, rouletteState, optionCustom, rouletteCustomState} = this.state
     return (
       <View style={{alignItems:"center"}}>
         <Text>
@@ -47,7 +53,21 @@ export default class App extends Component {
                   options={options}
                   markerWidth={20} >          
         </Roulette>
- 
+        <Text>
+          {`Option selected: ${optionCustom}`}
+        </Text>
+        <Text>
+          {`Roulette state: ${rouletteCustomState}`}
+        </Text>
+        <Roulette 
+                  enableUserRotate={rouletteCustomState=='stop'} 
+                  background={null}
+                  onRotate={this.onRotateCustom}
+                  onRotateChange={this.onRotateCustomChange}
+                  marker={marker}
+                  options={customOptions}
+                  markerWidth={20} >          
+        </Roulette>
       </View>
     );
   }
@@ -62,6 +82,19 @@ export default class App extends Component {
     
     this.setState({
       option:option.index
+    })
+  }
+
+  onRotateCustomChange(state) {
+    this.setState({
+      rouletteCustomState: state
+    })
+  }
+
+  onRotateCustom(option) {
+    
+    this.setState({
+      optionCustom:option.props.index
     })
   }
 }
